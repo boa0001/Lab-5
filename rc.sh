3b96a2e8d40494d4b93fb0187de79f4e6c2e2bc4
Laurie Stevison <lstevison@gmail.com>
2022-09-30 08:56:59 -0500

    Add files via upload

---
+++ rc.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+#initialize variable
+reverse=""
+
+#read in data
+name=`grep ">" $1`
+sequence=`grep -v ">" $1`
+
+echo "Name: $name"
+
+#calculate length of sequence
+len=${#sequence}
+
+echo "Length: $len"
+
+#loop through sequence in reverse
+for (( i=$len; i>=0; i-- ))
+do
+    reverse="$reverse${sequence:$i:1}"
+done
+
+echo "$sequence
+$reverse
+"
+
+#complement sequence
+rc=`echo $reverse | tr 'atcg' 'tagc'`
+
+echo "$rc
+"
+
+echo $name >$1.rc.txt
+echo $rc >>$1.rc.txt
