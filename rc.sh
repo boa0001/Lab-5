d4d1412bb8550c454d319bfa6cab8ae9ad446e92
Benjamin O. Allen <boa0001@auburn.edu>
2022-10-05 16:20:05 -0500

    q

--- rc.sh
+++ rc.sh
@@ -1,34 +1,37 @@
 #! /bin/sh
 ##Trial
 #initialize variable
+
+if [ $# -lt 1 ] || [ $# -gt 1  ]
+
+then
+	echo "You must input a single file!"
+else
+
 reverse=""
 
 #read in data
 name=`grep ">" $1`
 sequence=`grep -v ">" $1`
 
-echo "Name: $name"
+#echo "Name: $name"
 
 #calculate length of sequence
 len=${#sequence}
 
-echo "Length: $len"
+#echo "Length: $len"
 
 #loop through sequence in reverse
-for (( i=$len; i>=0; i-- ))
-do
-    reverse="$reverse${sequence:$i:1}"
-done
 
-echo "$sequence
-$reverse
-"
+reverse=`echo $sequence | rev`
+
+#echo "$sequence$reverse"
 
 #complement sequence
-rc=`echo $reverse | tr -I 'atcg' 'tagc'`
+rc=`echo $reverse | tr 'ATCG' 'TAGC'`
 
-echo "$rc
-"
+#echo "$rc"
 
-echo $name >$1.rc.txt
-echo $rc >>$1.rc.txt
+echo $name >$1.rc
+echo $rc >>$1.rc
+fi
